7904ea3f5a1771e13cdf6a0d4f4521078f1ec765
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-05-30 12:03:37 -0700

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-url: http://lonely-stretch.surge.sh
+### **URL**  
+http://lonely-stretch.surge.sh
+
+### **Project Description**
+A simple movie search app using Angular.js and the omdb API. Due to restrictions on the API, the movie posters do not always display properly.
--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'partials/home.html',
+      templateUrl: 'partials/home.html, partials/blah.html',
       controller: 'HomeController'
     })
     .when('/:id', {
